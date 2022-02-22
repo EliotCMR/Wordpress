079b37fa10683eebb926739630aad1d14848e9f5
Eliot <eliot.camara53@gmail.com>
2022-02-22 13:28:50 +0100

    Wordpress

---
+++ wp-content/themes/kawi/sidebar.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Kawi
+ */
+
+if ( ! is_active_sidebar( 'sidebar-1' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area" role="complementary">
+    <?php do_action( 'kawi_before_sidebar', 'sidebar-1' ) ?>
+	<div class="wrapper widgets-wrapper">
+		<?php dynamic_sidebar( 'sidebar-1' ); ?>
+	</div>
+	<?php do_action( 'kawi_after_sidebar', 'sidebar-1' ) ?>
+</aside><!-- #secondary -->

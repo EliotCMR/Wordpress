57b0ba3dadd77519d4e2fcbd979cfc1c902214fd
Eliot <eliot.camara53@gmail.com>
2022-03-22 16:07:08 +0100

    theme wordpress

---
+++ wp-content/themes/montheme/functions.php
@@ -0,0 +1,44 @@
+<?php
+
+function montheme_supports()
+{
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    add_theme_support('menus');
+    register_nav_menu('header', 'En tête du menu');
+    register_nav_menu('footer', 'Pied de page');
+}
+
+function montheme_register_assets()
+{
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css', [], 1.2);
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js', [], false, true);
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', 'https://code.jquery.com/jquery-3.2.1.slim.min.js', [], false, true);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
+function montheme_title_separator()
+{
+    return '|';
+}
+
+function montheme_menu_class($classes)
+{
+    $classes[] = 'nav-item';
+    return $classes;
+}
+
+function montheme_menu_link_class($attrs)
+{
+    $attrs['class'] = 'nav-link';
+    return $attrs;
+}
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assets');
+add_filter('document_title_separator', 'montheme_title_separator');
+add_filter('nav_menu_css_class', 'montheme_menu_class');
+add_filter('nav_menu_link_attributes', 'montheme_menu_link_class');
---
+++ wp-content/themes/montheme/index.php
@@ -0,0 +1,26 @@
+<?php get_header() ?>
+
+<?php if (have_posts()) : ?>
+
+    <div class="row">
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card">
+                    <?php the_post_thumbnail('post-thumbnail', ['class' => 'card-img-top', 'alt' => '', 'style' => 'height: auto;']) ?>
+
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?></h5>
+                        <h6 class="card-subtitle mb-2 text-muted"><?php the_category() ?></h6>
+                        <p class="card-text"><?php the_content('Voir plus') ?></p>
+
+                    </div>
+                </div>
+            </div>
+        <?php endwhile ?>
+    </div>
+
+<?php else : ?>
+    <h1>Pas d'articles</h1>
+<?php endif; ?>
+
+<?php get_footer() ?>
